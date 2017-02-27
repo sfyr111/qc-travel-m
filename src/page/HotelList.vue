4254d2eb0e7cd05bf9451b1108175f790aa5a461
young <from_yangxin@126.com>
2017-02-27 18:41:13 +0800

    提交新组件

---
+++ src/page/HotelList.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<!-- 城市，酒店名 -->
+		<hotel-list-head></hotel-list-head>
+
+		<!-- 酒店列表 -->
+		<hotel-list></hotel-list>
+	</div>
+</template>
+
+<script>
+import { HotelList, HotelListHead } from '../components'
+
+export default {
+	data () {
+		return {}
+	},
+
+	components: {
+		HotelList,
+		HotelListHead
+	}
+}
+</script>
+
+<style lang="less">
+	body{
+		background: #fff;
+	}
+</style>
